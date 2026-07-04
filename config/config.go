0c190f2c73ffd9216f584467351f87bedb5194f1
Tony NGUEREZA <nguerezatony@gmail.com>
2026-07-04 08:52:51 +0100

    Update config feature

--- config/config.go
+++ config/config.go
@@ -1,94 +1,180 @@
 package config
 
+import (
+	"strings"
+	"sync"
+
+	"github.com/joho/godotenv"
+	"github.com/spf13/viper"
+)
+
 type RedisConfig struct {
-	Host         string
-	Port         int
-	Password     string
-	DB           int
-	PoolSize     int
-	MinIdleConns int
-	DialTimeout  int
-	ReadTimeout  int
-	WriteTimeout int
-	MaxRetries   int
+	Host         string `mapstructure:"host"`
+	Port         int    `mapstructure:"port"`
+	Password     string `mapstructure:"password"`
+	DB           int    `mapstructure:"db"`
+	PoolSize     int    `mapstructure:"pool_size"`
+	MinIdleConns int    `mapstructure:"min_idle_conns"`
+	DialTimeout  int    `mapstructure:"dial_timeout"`
+	ReadTimeout  int    `mapstructure:"read_timeout"`
+	WriteTimeout int    `mapstructure:"write_timeout"`
+	MaxRetries   int    `mapstructure:"max_retries"`
+}
+
+type ServerConfig struct {
+	Host        string `mapstructure:"host"`
+	Port        int    `mapstructure:"port"`
+	Mode        string `mapstructure:"mode"`
+	DocsEnabled bool   `mapstructure:"docs_enabled"`
 }
 
 type AuthConfig struct {
-	JwtSecret     string
-	JwtAccessTTL  int // seconds
-	JwtRefreshTTL int // seconds
+	JwtSecret     string `mapstructure:"jwt_secret"`
+	JwtAccessTTL  int    `mapstructure:"jwt_expire_time"`         // seconds
+	JwtRefreshTTL int    `mapstructure:"jwt_refresh_expire_time"` // seconds
 }
 
 type MemoryCacheConfig struct {
-	CleanupInterval int
-	MaxItems        int
+	CleanupInterval int `mapstructure:"cleanup_interval"`
+	MaxItems        int `mapstructure:"max_items"`
 }
 
 type CacheConfig struct {
-	Type       string
-	Prefix     string
-	DefaultTTL int
-	Redis      RedisConfig
-	Memory     MemoryCacheConfig
+	Type       string            `mapstructure:"type"`
+	Prefix     string            `mapstructure:"prefix"`
+	DefaultTTL int               `mapstructure:"default_ttl"`
+	Redis      RedisConfig       `mapstructure:"redis"`
+	Memory     MemoryCacheConfig `mapstructure:"memory"`
 }
 
 type DatabaseConfig struct {
-	Driver          string // mysql, postgres, sqlite, sqlserver
-	Host            string
-	Port            int
-	User            string
-	Password        string
-	DBName          string
-	SSLMode         string
-	MaxOpenConns    int
-	MaxIdleConns    int
-	ConnMaxLifetime int // seconds
+	Driver          string `mapstructure:"driver"` // mysql, postgres, sqlite, sqlserver
+	Host            string `mapstructure:"host"`
+	Port            int    `mapstructure:"port"`
+	User            string `mapstructure:"user"`
+	Password        string `mapstructure:"password"`
+	DBName          string `mapstructure:"dbname"`
+	SSLMode         string `mapstructure:"sslmode"`
+	MaxOpenConns    int    `mapstructure:"max_open_conns"`
+	MaxIdleConns    int    `mapstructure:"max_idle_conns"`
+	ConnMaxLifetime int    `mapstructure:"conn_max_lifetime"` // seconds
 }
 
 type EmailConfig struct {
-	Provider string
-	SMTP     SMTPConfig
+	Provider string     `mapstructure:"provider"`
+	SMTP     SMTPConfig `mapstructure:"smtp"`
 }
 
 type SMTPConfig struct {
-	Host       string
-	Port       int
-	Username   string
-	Password   string
-	From       string
-	Timeout    int    // seconds
-	Encryption string // none, tls, starttls
+	Host       string `mapstructure:"host"`
+	Port       int    `mapstructure:"port"`
+	Username   string `mapstructure:"username"`
+	Password   string `mapstructure:"password"`
+	From       string `mapstructure:"from"`
+	Timeout    int    `mapstructure:"timeout"`    // seconds
+	Encryption string `mapstructure:"encryption"` // none, tls, starttls
 }
 
 type LoggingConfig struct {
-	Level      string
-	Format     string
-	FilePath   string
-	MaxSizeMB  int
-	MaxBackups int
-	MaxAgeDays int
-	Compress   bool
+	Level      string `mapstructure:"level"`
+	Format     string `mapstructure:"format"`
+	FilePath   string `mapstructure:"file_path"`
+	MaxSizeMB  int    `mapstructure:"max_size_mb"`
+	MaxBackups int    `mapstructure:"max_backups"`
+	MaxAgeDays int    `mapstructure:"max_age_days"`
+	Compress   bool   `mapstructure:"compress"`
 }
 
 type StorageConfig struct {
-	Type      string
-	Endpoint  string
-	Bucket    string
-	AccessKey string
-	SecretKey string
-	UseSSL    bool
+	Type      string `mapstructure:"type"`
+	Endpoint  string `mapstructure:"endpoint"`
+	Bucket    string `mapstructure:"bucket"`
+	AccessKey string `mapstructure:"access_key"`
+	SecretKey string `mapstructure:"secret_key"`
+	UseSSL    bool   `mapstructure:"use_ssl"`
 }
 
 type AntiReplayConfig struct {
-	Enabled            bool
-	TimestampTolerance int // minutes
-	CacheTTL           int // seconds
+	Enabled            bool `mapstructure:"enabled"`
+	TimestampTolerance int  `mapstructure:"timestamp_tolerance"` // minutes
+	CacheTTL           int  `mapstructure:"cache_ttl"`           // seconds
 }
 
 type CORSConfig struct {
-	AllowedOrigins   []string
-	AllowedMethods   []string
-	AllowedHeaders   []string
-	AllowCredentials bool
-	MaxAge           int
+	AllowedOrigins   []string `mapstructure:"allowed_origins"`
+	AllowedMethods   []string `mapstructure:"allowed_methods"`
+	AllowedHeaders   []string `mapstructure:"allowed_headers"`
+	AllowCredentials bool     `mapstructure:"allow_credentials"`
+	MaxAge           int      `mapstructure:"max_age"`
+}
+
+type RateLimitConfig struct {
+	Enabled                  bool `mapstructure:"enabled"`
+	DefaultRequestsPerMinute int  `mapstructure:"default_requests_per_minute"`
+	Burst                    int  `mapstructure:"burst"`
+}
+
+type Config struct {
+	Server     ServerConfig     `mapstructure:"server"`
+	CORS       CORSConfig       `mapstructure:"cors"`
+	Logging    LoggingConfig    `mapstructure:"database"`
+	Database   DatabaseConfig   `mapstructure:"database"`
+	Auth       AuthConfig       `mapstructure:"auth"`
+	Cache      CacheConfig      `mapstructure:"cache"`
+	Storage    StorageConfig    `mapstructure:"storage"`
+	Email      EmailConfig      `mapstructure:"email"`
+	RateLimit  RateLimitConfig  `mapstructure:"rate_limit"`
+	AntiReplay AntiReplayConfig `mapstructure:"antireplay"`
+}
+
+var (
+	configInstance *Config
+	configOnce     sync.Once
+)
+
+func NewConfig(configFilePath, configDir string) *Config {
+	configOnce.Do(func() {
+		configInstance = loadConfig(configDir, configFilePath)
+	})
+	return configInstance
+}
+
+func loadConfig(configDir, configFilePath string) *Config {
+	loadDotEnv()
+	setupViper(configDir, configFilePath)
+	readConfigFile()
+	bindEnvVariables()
+
+	var cfg Config
+	if err := viper.Unmarshal(&cfg); err != nil {
+		panic(err)
+	}
+
+	return &cfg
+
+}
+
+func loadDotEnv() {
+	_ = godotenv.Load()
+}
+
+func setupViper(configDir, configFilePath string) {
+	if configFilePath != "" {
+		viper.SetConfigFile(configFilePath)
+	} else {
+		viper.SetConfigName("config")
+		viper.SetConfigType("yaml")
+		viper.AddConfigPath(configDir)
+		viper.AddConfigPath(".")
+	}
+}
+
+func readConfigFile() {
+	_ = viper.ReadInConfig()
+}
+
+func bindEnvVariables() {
+	viper.SetEnvPrefix("APP")
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.AutomaticEnv()
 }
--- go.mod
+++ go.mod
@@ -9,11 +9,13 @@ require (
 	github.com/go-playground/validator/v10 v10.30.1
 	github.com/golang-jwt/jwt/v5 v5.2.2
 	github.com/google/uuid v1.6.0
+	github.com/joho/godotenv v1.5.1
 	github.com/minio/minio-go/v7 v7.2.1
 	github.com/natefinch/lumberjack v2.0.0+incompatible
 	github.com/prometheus/client_golang v1.23.2
 	github.com/redis/go-redis/v9 v9.21.0
 	github.com/sirupsen/logrus v1.9.4
+	github.com/spf13/viper v1.12.0
 	golang.org/x/crypto v0.53.0
 	gorm.io/driver/mysql v1.6.0
 	gorm.io/driver/postgres v1.6.0
@@ -31,6 +33,7 @@ require (
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/cloudwego/base64x v0.1.6 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/fsnotify/fsnotify v1.5.4 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.12 // indirect
 	github.com/gin-contrib/sse v1.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
@@ -40,6 +43,7 @@ require (
 	github.com/goccy/go-yaml v1.19.2 // indirect
 	github.com/golang-sql/civil v0.0.0-20220223132316-b832511892a9 // indirect
 	github.com/golang-sql/sqlexp v0.1.0 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.9.2 // indirect
@@ -51,14 +55,17 @@ require (
 	github.com/klauspost/cpuid/v2 v2.3.0 // indirect
 	github.com/klauspost/crc32 v1.3.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/magiconair/properties v1.8.6 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-sqlite3 v1.14.22 // indirect
 	github.com/microsoft/go-mssqldb v1.8.2 // indirect
 	github.com/minio/crc64nvme v1.1.1 // indirect
 	github.com/minio/md5-simd v1.1.2 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.3.1 // indirect
 	github.com/philhofer/fwd v1.2.0 // indirect
 	github.com/prometheus/client_model v0.6.2 // indirect
@@ -67,6 +74,11 @@ require (
 	github.com/quic-go/qpack v0.6.0 // indirect
 	github.com/quic-go/quic-go v0.59.1 // indirect
 	github.com/rs/xid v1.6.0 // indirect
+	github.com/spf13/afero v1.15.0 // indirect
+	github.com/spf13/cast v1.5.0 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.10 // indirect
+	github.com/subosito/gotenv v1.4.1 // indirect
 	github.com/tinylib/msgp v1.6.1 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.3.1 // indirect
@@ -85,4 +97,6 @@ require (
 	gopkg.in/ini.v1 v1.67.2 // indirect
 	gopkg.in/mail.v2 v2.3.1 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
